a75a5ad4bf9aec4df577f70c145c0ea62f208b8a
Martin Jusbicher <jusbichermartin@gmail.com>
2022-07-15 20:52:08 -0300

    card detail ajustes

--- src/components/MainContent/ItemDetail.jsx
+++ src/components/MainContent/ItemDetail.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { Card } from "react-bootstrap";
+import styles from "./MediaCardDetail.module.css"
+import Button from 'react-bootstrap/Button';
 
 
 const ItemDetail = ({ data }) => {
   return (
     <>
-      <Card className="text-white" style={{backgroundColor: "black"}}>
-      <Card.Img src={data.cover} alt="Card image" />
-        <h1>{data.name}</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus laborum sequi, fugiat tempora, eveniet adipisci quia ut rerum numquam ipsam in ea perferendis suscipit sunt doloribus, ad repellat officiis ducimus?
-        </p>
+      <Card className={[styles.cardDetail, styles.cardDetailMobile]} style={{backgroundColor: "black"}}>
+      <Card.Img className={styles.cardDetailImg} src={data.cover} alt="Card image" />
+        <div className={styles.cardDetailText}>
+          <h1>{data.name}</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus laborum sequi, fugiat tempora, eveniet adipisci quia ut rerum numquam ipsam in ea perferendis suscipit sunt doloribus, ad repellat officiis ducimus?
+          </p>
+          <Button style={{background: '#e50914', borderColor: '#e50914'}} active>
+            Añadir a Favoritos
+          </Button>
+        </div>
     </Card>
     </>
   );
