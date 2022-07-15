a6e91fc40610b1b882e85b96a32728761c59aeca
Martin Jusbicher <jusbichermartin@gmail.com>
2022-07-15 19:47:30 -0300

    Agregado mas lista de peliculas

--- src/data.js
+++ src/data.js
@@ -1,50 +1,265 @@
 export const data = [
-    {
-        id: 1,
-        name: "Peaky Blinders",
-        category: "accion",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVjDzcNn4GkVdYYG1Mox6OBAxR36XqvP0ndhwfByOw-epU1pvAbL-8-6w_6qjg2rSgYJjTS0no11tjAQY4Cld4PpbkdT8QEvcegg87rTB1SaNK8XrwXLAL72B6cxrvMrI-ti.jpg?r=b23"
-    },
-    {
-        id: 2,
-        name: "Baki",
-        category: "accion",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTFnrubQ0XSYFrwnUIuJr52QAeyQkbBSb8yxO5F8-_2-lQy61xkSGAOiyJ5G9jEbiBryqlfzYxTKTzj3RC9ESQYQwzraYFJ5o3Kk2STv_BH3tCyUb76-G5YJUqaQI1s8qItA.jpg?r=37b"
-    },
-    {
-        id: 3,
-        name: "Black Mirror",
-        category: "drama",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABe3BWChn_kxOhUaAb_YVXWUxMBPfU0Oedo3w2ts_id074MOr29KbWyUMKGgtHp7jXdVd-dNubIWdSNRuTTDHUQVtM4TWLlY8zcFBtlNKeE8UydngwwBsBoTsFP7s99uZ-bzq.jpg?r=743"
-    },
-    {
-        id: 4,
-        name: "No miren arriba",
-        category: "comedia",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABc-IW06HAisy97YY0Q0ivM5F2_sXj7OJJd2GYsjJ_pF_kj6hsYY4aFuT84kZellvcSuET9fnVSZ3D8w3LJqWZeNBJIGB__iUpCTthHmXLC_yqBxanOH_TAxGpFXEiSEH6KFB.jpg?r=9be"
-    },
-    {
-        id: 5,
-        name: "Vikings",
-        category: "accion",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeMo_o3o7hns0PicB5uWNHT2t2nbJI_tkuaK_l-7GJcmotQl0Foa1jeU56fWnniGOhS1NCk2QCDFT01aziqB0VFZCG63m-VN-B8.jpg?r=21a"
-    },
-    {
-        id: 6,
-        name: "Black Mirror",
-        category: "drama",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeyI0TXUh_cpP9H-4zFYPZRwCj3qd_zhpoUjKeIR_mqpybGeeZTTl8M-_JPobg1Z83P42O05HWpDpJ1J96thR6lU7DrfIWZQamZfL6z3jkR5wadn5-ejldXHSF032o2EJbMK.jpg?r=fb7"
-    },
-    {
-        id: 7,
-        name: "The Flash",
-        category: "accion",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABY3C16Ea3fo4T2h04Q7VZZhDofoeWVFWhF-g1AdT7yqIdWsZtep1MXN0j-XFM2LZ1zLRkLXPx7k3o7qpJcB4AG3G0NHceunZYvE.jpg?r=615"
-    },
-    {
-        id: 8,
-        name: "Jackass",
-        category: "comedia",
-        image: "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZ6Wjz_KEkMc5_Y4A4jGY2fY8aEDgO4UOx2bfa2krfPEzMHaS4hLuKZunPh7mps0goOU1LhiJkr6KtJFTjAO71mAa8wMZjn62iQ.jpg?r=c6f"
+  {
+    id: 1,
+    name: "Peaky Blinders",
+    type: "serie",
+    category: "accion",
+    cover: "https://pbs.twimg.com/media/FSqtp_9X0AA1l13.jpg:large",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVjDzcNn4GkVdYYG1Mox6OBAxR36XqvP0ndhwfByOw-epU1pvAbL-8-6w_6qjg2rSgYJjTS0no11tjAQY4Cld4PpbkdT8QEvcegg87rTB1SaNK8XrwXLAL72B6cxrvMrI-ti.jpg?r=b23",
+  },
+  {
+    id: 2,
+    name: "Baki",
+    type: "serie",
+    category: "accion",
+    cover: "https://es.web.img3.acsta.net/pictures/19/09/24/14/21/3795092.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTFnrubQ0XSYFrwnUIuJr52QAeyQkbBSb8yxO5F8-_2-lQy61xkSGAOiyJ5G9jEbiBryqlfzYxTKTzj3RC9ESQYQwzraYFJ5o3Kk2STv_BH3tCyUb76-G5YJUqaQI1s8qItA.jpg?r=37b",
+  },
+  {
+    id: 3,
+    name: "Black Mirror",
+    type: "serie",
+    category: "drama",
+    cover: "https://www.lavanguardia.com/peliculas-series/images/serie/poster/2011/12/w1280/7PRddO7z7mcPi21nZTCMGShAyy1.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABe3BWChn_kxOhUaAb_YVXWUxMBPfU0Oedo3w2ts_id074MOr29KbWyUMKGgtHp7jXdVd-dNubIWdSNRuTTDHUQVtM4TWLlY8zcFBtlNKeE8UydngwwBsBoTsFP7s99uZ-bzq.jpg?r=743",
+  },
+  {
+    id: 4,
+    name: "No miren arriba",
+    type: "movie",
+    category: "comedia",
+    cover: "https://d1fmx1rbmqrxrr.cloudfront.net/cnet/i/edit/2021/11/dont-look-up-poster.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABc-IW06HAisy97YY0Q0ivM5F2_sXj7OJJd2GYsjJ_pF_kj6hsYY4aFuT84kZellvcSuET9fnVSZ3D8w3LJqWZeNBJIGB__iUpCTthHmXLC_yqBxanOH_TAxGpFXEiSEH6KFB.jpg?r=9be",
+  },
+  {
+    id: 5,
+    name: "Vikings",
+    type: "serie",
+    category: "accion",
+    cover: "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/1200/public/media/image/2022/01/poster-vikingos-valhalla-2589483.jpg?itok=m3YEwX5e",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeMo_o3o7hns0PicB5uWNHT2t2nbJI_tkuaK_l-7GJcmotQl0Foa1jeU56fWnniGOhS1NCk2QCDFT01aziqB0VFZCG63m-VN-B8.jpg?r=21a",
+  },
+  {
+    id: 6,
+    name: "The Witcher",
+    type: "serie",
+    category: "accion",
+    cover: "https://xmag.live/wp-content/uploads/2019/07/The-Witcher_Vertical-Teaser_RGB_ES.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcatVGW_l7usfiP0Mv9JLlxhL876CFdXrCdlHL1EUrZyMlvDLOX4T-WilPECSr6k7_66QH2z7jUZYzDogu4PKOfuzX5ndqCTZuT8yYgJtfKDKX5zUnCrfGns6C4KYI4nxxSg.jpg?r=592",
+  },
+  {
+    id: 7,
+    name: "The Flash",
+    category: "accion",
+    type: "serie",
+    cover: "https://fanmazing.files.wordpress.com/2015/10/the-flash-novo-poster.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABY3C16Ea3fo4T2h04Q7VZZhDofoeWVFWhF-g1AdT7yqIdWsZtep1MXN0j-XFM2LZ1zLRkLXPx7k3o7qpJcB4AG3G0NHceunZYvE.jpg?r=615",
+  },
+  {
+    id: 8,
+    name: "Jackass",
+    category: "comedia",
+    type: "movie",
+    cover: "https://image.tmdb.org/t/p/original/bl686OSeMQOvs90IQAgt8QipCsB.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZ6Wjz_KEkMc5_Y4A4jGY2fY8aEDgO4UOx2bfa2krfPEzMHaS4hLuKZunPh7mps0goOU1LhiJkr6KtJFTjAO71mAa8wMZjn62iQ.jpg?r=c6f",
+  },
+  {
+    id: 9,
+    name: "El Irlandés",
+    category: "drama",
+    type: "movie",
+    cover: "https://www.sidewalkfest.com/wp-content/uploads/2019/11/The-Irishman-2019-650x952.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABekN5sx8PW012rGY989YWWf77VfNI-x6iOlJOEJTrV8g7Ydz8gUgpvo6Epnb-3tdrEPrkUsMf6PlabCGc_9sF4blvXD9eGTVv1ZRmrfiRuwNQRgnglqtnJDRVB60Xvy_-ER-.jpg?r=272",
+  },
+  {
+    id: 10,
+    name: "El Hoyo",
+    category: "drama",
+    type: "movie",
+    cover: "https://monterreyrock.com/wp-content/uploads/2020/03/el-hoyo-poster-netflix-scaled.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQdi4Bp5VgCEPRoFFTdgva3rTbGb2F_gOei4m8PHHjvPfm2F8LdpWoIdwWn3PG-Z1HKhixFP5zID4Kp8fYBTgsfZiIBd7PE6TXrqTWxo7dT59t5z9LSPUN6-Igkv76Ps_3ym.jpg?r=4fe",
+  },
+  {
+    id: 11,
+    name: "Élite",
+    category: "drama",
+    type: "serie",
+    cover: "https://pbs.twimg.com/media/E4GYGm4WUAoM-_n?format=jpg&name=4096x4096",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABeh5zF1P6D9mPIuOFuZP0AvEjcEA2NLgGcnFr0PPYhVbMATm8q4QJsO5cggB4790hk1j7xcP4kylk-kKu61NiAxWotK4TJU9mHmSMYa62cxxY6VcUWM7ZGKLWKheqpED0V2_.jpg?r=f87",
+  },
+  {
+    id: 12,
+    name: "La Maldición de Hill House",
+    category: "drama",
+    type: "serie",
+    cover: "https://es.web.img2.acsta.net/pictures/18/09/20/08/44/5720467.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABfd-AAwXe5H21L0wwiQPdgwTxa6VpCJkp_pvxflaQ6ulp-i-gretqsSh1S5-krZZ1zve4TfvcAKWl0bkDT-tFk68LYmLyupom9ppyNQSbgnXuDAU5OpxfiatapIpTsMHB1X9.jpg?r=063",
+  },
+  {
+    id: 13,
+    name: "Dark",
+    category: "drama",
+    type: "serie",
+    cover: "https://www.bolsamania.com/seriesadictos/wp-content/uploads/2017/11/3064798.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABbetdjjuRqow-y1xvgeBLmzwbRnwkmAZW4PApMgT6h5tYSa2ZpTkpwm9JJ3ZCvQ2FDUTeriOnWB2422V094CEg_hc8C7gU2vNK4fAdsbWhXTlJnwlDiXy9hw5i3dbrEo0-Nc.jpg?r=393",
+  },
+  {
+    id: 14,
+    name: "Naruto Shippuden",
+    category: "accion",
+    cover: "https://m.media-amazon.com/images/M/MV5BMTE5NzIwMGUtYTE1MS00MDUxLTgyZjctOWVkZDAxM2M4ZWQ4XkEyXkFqcGdeQXVyNjc2NjA5MTU@._V1_FMjpg_UX1000_.jpg",
+    type: "serie",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABcYM77ftQridIJZa2MV7igFwD2mj9kZ1B6jnJ8kACBh3trcc7jhY1HyggkxY8MNZH33hjaXqbwb5Pa2RjRELjs-zu3QBpj-79RQ.jpg?r=5ac",
+  },
+  {
+    id: 15,
+    name: "Rick y Morty",
+    category: "comedia",
+    type: "serie",
+    cover: "https://www.cinematyrant.com/wp-content/uploads/2016/10/Rick-and-Morty-on-netflix-700x905.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSvOjibX_bLMDeJ_PbWQBqJZv6QabWTOjlP5eJWIh9n_LgkDBwVc56pMoIsHbp2s_4wVHsafiB3mDeocNWHw9PXnunVR8pT5fs0.jpg?r=a4a",
+  },
+  {
+    id: 16,
+    name: "Shrek",
+    category: "comedia",
+    type: "movie",
+    cover: "https://i2.netflixmovies.com/dibsl9ebc/image/upload/w_600,h_891,c_lfill,g_north/fs4k8nykkee2bmsvxvk7.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZtSZaljkNC4lsYh4_Zj0-O0BJqMSAJOqu-dwl1bjKcIyBk0u7KdQAFmh7z9Icn58XMds--HU9Xy6pb5g0-4f1NZsrhK8aei5jQ.jpg?r=e24",
+  },
+  {
+    id: 17,
+    name: "El Club de la Pelea",
+    category: "accion",
+    cover: "https://images.justwatch.com/poster/66105154/s718/el-club-de-la-lucha.%7Bformat%7D",
+    type: "movie",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVgyKfbnkYMbrwWzFgLksYxfl-hYF-o8NdX1F4mbUcr3T_5oBH_pUv3Cw_uDEE09iWIDGB35PfA_ymGRAgSiZ9d9AlX51uj5nfY.jpg?r=054",
+  },
+  {
+    id: 18,
+    name: "El Efecto Mariposa",
+    category: "drama",
+    type: "movie",
+    cover: "https://occ-0-1722-1723.1.nflxso.net/art/c69aa/e9f23a1540a6bf5384ea1db7c4b7b8c4d9ec69aa.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABSTH7DYnzRtAXFjj-ss5LVer-Pbb-qD-7yfEVDVnGZenTm9vf3KwAaKvE_FsHUj7jb-jmii9PqMFcWEtUpmOhQrkTFWz4IT8Bv8.jpg?r=b3c",
+  },
+  {
+    id: 19,
+    name: "Spiderman Into The Spider-Verse",
+    category: "comedia",
+    type: "accion",
+    cover: "https://musicart.xboxlive.com/7/9af75000-0000-0000-0000-000000000002/504/image.jpg?w=1920&h=1080",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABdvZFmzXLsVRkzWOpHNNYkcQ9qzuOI4Jxag7nzwlMEFvEMvjUiiPaEBKOmqIRfbIn2I3oDSrYimS6H67Tc8sZrZWmW9be4zwqx4.jpg?r=7f9",
+  },
+  {
+    id: 20,
+    name: "El Marginal",
+    category: "drama",
+    type: "serie",
+    cover: "https://m.media-amazon.com/images/M/MV5BYTcxMjZiZWUtZGI2Ny00NTMzLWFkNzMtOTk1Y2FlZjMzZTQ2XkEyXkFqcGdeQXVyMTEzMTI1Mjk3._V1_.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABRoo51QvA4z22QYRjyixos1RPg24LqRoGlUEEPtBJ0VyKQ8QMkvV2FCUipWHbdSOHVA7ENMu-B9uHn1lT4Xs8nRAHANZENw7NLU5y1dUOuCKNKRdxiOZslChFfcSjjAIJykz.jpg?r=fd4",
+  },
+  {
+    id: 21,
+    name: "Los Simuladores",
+    category: "drama",
+    type: "serie",
+    cover: "https://images.justwatch.com/poster/189121392/s718/los-simuladores.%7Bformat%7D",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABZHOUhCdE3wFQu9nbZahni6L1x5cF02sezxKhSP9sV2rubTgJLpbXNXHiYE8t_bdh93G2sy8vTazNXMd1hNoQoXj2oUYCaMTU-o.jpg?r=2a9",
+  },
+  {
+    id: 22,
+    name: "Fumados",
+    category: "comedia",
+    type: "serie",
+    cover: "https://pbs.twimg.com/media/EjByamJUwAExH7m.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABTWI5S52gcJO0CSjXt3k0NYUMw_3phMlbmSrt2Ay-ok0BUJz8lH_CXN_QjaVgw2msxxHgLYwLd-GTO23KLpI__MV-Tz3z_iY35c2pP8tBj-OsBN_iGiikhh2NhG07CdgPL7I.jpg?r=2ad",
+  },
+  {
+    id: 23,
+    name: "How To Sell Drugs Online",
+    category: "comedia",
+    type: "serie",
+    cover: "https://pics.filmaffinity.com/How_to_Sell_Drugs_Online_Fast_Serie_de_TV-351581647-large.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABQLnEp73Qg8U78HBXwHBhzRS7eAQIKohG5yTJ959CPr4OfS3vR5PpYbF1fGFks0HpBTUfXtyT5oUW_jGz4_SZn5KZRXUUNAbViNt2Vfdm6tfbNNnOXX6z2LVuzg-YZeED1Vs.jpg?r=53c",
+  },
+  {
+    id: 24,
+    name: "Sex Education",
+    category: "comedia",
+    type: "serie",
+    cover: "https://idolosolvidados.com/wp-content/uploads/2020/01/LJL5LWAPQREYBGXEEXT23NKA54.jpg",
+    image:
+      "https://occ-0-5363-2430.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABV8KjLSOBc6Y7NeN1oXOFc2hJwXlKfKYQfMu78TgXKmaObvgrunUXtbwL-JuKDIUJA5j_EBL7AIPhvgTDK_JMV6MJ-L9B-Ex3WEfpbeK5rM8mBjIrW_5UnauKLh_kyIXI0BJ.jpg?r=41a",
+  },
+  {
+    id: 25,
+    name: "Son Como Niños",
+    category: "comedia",
+    type: "movie",
+    cover: "https://i2.netflixmovies.com/dibsl9ebc/image/upload/w_600,h_891,c_lfill,g_north/jbagmzublklgnb1ovvxt.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABXdH7uv85WGKpIjNgoy0fjrYeByivT26ZjIHBDJiYZU89EplitUCbAZgFZta5MOso3c83Q8E_90hYMUs9CEodvpIyXcIs9tHxDQ.jpg?r=d81",
+  },
+  {
+    id: 26,
+    name: "John Wick 3",
+    category: "accion",
+    type: "movie",
+    cover: "https://images-na.ssl-images-amazon.com/images/I/91f2bgjN6EL._RI_.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABVbAL2bRZa7CfW5_kcwDwd2e-IC1VAmT1e4rH4qHReYKOIbu-ufym2paIBlSHGt_oDpVQ8tuUudx-vqaICrWoEXS3Ws7ix0h7aQ.jpg?r=c62",
+  },
+  {
+    id: 27,
+    name: "Triple Frontera",
+    category: "accion",
+    type: "movie",
+    cover: "https://www.moviementarios.com/wp-content/uploads/2019/03/Triple-Frontera-Cartel.jpg",
+    image:
+      "https://occ-0-5363-1740.1.nflxso.net/dnm/api/v6/X194eJsgWBDE2aQbaNdmCXGUP-Y/AAAABUPRB6NDqUuWzcgJ8n9YrR_aOXJi56fjVaBXCiTUxUTTl4-Dx5CDpu82oR2eFiy8QAPoXjAu-gMWkgJtxXlRTVBUeh-m1b9XqXACkKiKDuhagHO8ireo32GQJispJAmsiRpj.jpg?r=47b",
+  },
+];
+
+export const traerData = (categoryId) => {
+  return new Promise((resolve, reject) => {
+    if (categoryId) {
+      const dataFilter = data.filter((data) => data.category === categoryId);
+      resolve(dataFilter);
     }
-]
+    resolve(data);
+  }, 2000);
+};
+
+export const dataById = (id) => {
+  return new Promise((resolve, reject) => {
+    if (id) {
+      const dataFilterById = data.find((data) => data.id === Number(id));
+      resolve(dataFilterById);
+    }
+    resolve(data);
+  }, 2000);
+};
